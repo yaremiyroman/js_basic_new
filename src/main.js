3f7fb01c7c81834dd30c60a54a77b1270c8661ee
andrdrx <yaremiyroman@gmail.com>
2024-04-08 21:14:33 +0300

    4

--- src/main.js
+++ src/main.js
@@ -1,1 +1,1157 @@
-console.log("Hey, this is my first JS program");
+// // 'sdfgsdfgsdfg';
+// // 'string';
+// // '';
+
+// // "another string";
+
+// // const a = 'some string';
+
+// // console.log(a);
+
+
+
+// // const newString = 'some string';
+// // const newString2 = "some string 2";
+
+// // So called Camel case
+// // soCalledCamelCase
+
+
+
+// // const str = 'My "new" string';
+// // const str2 = "Another 'string' new one"
+// // console.log(str);
+// // console.log(str2);
+
+
+// // const str4 = 'Some text';
+// // const str5 = 'Some another text';
+// // // const str6 = 'Some another text 6666';
+
+// // const stringLiteral = `${3 + 4} Curve ${str5} braces ${str5}`;
+
+// // console.log(stringLiteral);
+
+// // 'Lorem ipsum dolor sit amet consectetur'
+
+// // "Lorem ipsum dolor sit amet consectetur"
+
+// // const manyString = `Lorem ipsum dolor
+// //      sit amet
+// //      consectetur`;
+
+// // console.log(manyString);
+// // console.log(manyString.length);
+// // console.log(manyString[4]);
+
+
+
+
+
+// // const myString = '431';
+// // console.log(myString);
+
+// // const myNumber = 431;
+// // const myNumber2 = 100;
+// // console.log(myNumber);
+// // console.log(myNumber2);
+
+// // console.log(myNumber, myNumber2);
+
+// // console.log('myNumber2 > ', myNumber2);
+
+// // console.log(1000);
+// // console.log(1000 + 250);
+// // console.log(1000 + 250, 'stroka');
+
+
+// // const str1 = 'Pevna stroka';
+
+// // const str2 = '';
+
+// // console.log(str1.length);
+// // console.log(str2.length);
+
+
+// // const number1 = 400;
+// // console.log(number1);
+
+// // const number2 = 400.5;
+// // console.log(number2);
+
+// // const number3 = -400.5;
+// // console.log(number3);
+
+// // const number4 = -800;
+// // console.log(number4);
+
+// // const number5 = 1;
+// // console.log(number5);
+
+// // const numberZero = 0;
+// // console.log(numberZero);
+
+// // const number9 = -0.00043;
+// // console.log(number9);
+
+
+
+// // const summa = 5 + 9;
+// // console.log(summa);
+
+// // const summa2 = -5 / 9;
+// // console.log(summa2);
+
+// // const infin = +Infinity;
+// // const minusInfin = -Infinity;
+// // const notNumber = NaN;
+
+// // console.log(15 / 0);
+
+// // 0 1 
+// // 1 2 3 4 5 6 7 8 9 0
+// // 1 2 3 4 5 6 7 8 9 0 a b c d e f 
+
+// // const billionLetters = 10000000000;
+// // const billion = 1e9;
+// // const negativeBillion = 1e-9;
+// // console.log(billionLetters, billion, negativeBillion);
+
+// // const a = 0b11111111; // двійкова форма 255
+
+// // string
+// // number
+// // BigInt
+
+
+// // const anyNumber = 1111n;
+// // console.log('anyNumber > ', anyNumber);
+
+// // const anyNumber2 = 2222n;
+// // console.log('anyNumber2 > ', anyNumber2);
+
+// // console.log(anyNumber + anyNumber2);
+
+
+// // boolean
+
+// // const first = true;
+// // const second = false;
+
+// // console.log('first > ', first);
+
+
+
+// // undefined
+
+// // const undef = undefined;
+// // console.log('undef > ', undef);
+
+// // let newUndef;
+// // console.log('newUndef > ', newUndef);
+
+
+// // const a = 5;
+// // a = 100;
+// // console.log(a);
+
+// // let b = 7;
+// // console.log(b);
+
+// // b = 10;
+// // console.log(b);
+
+
+
+// // null
+
+// // const undefVariable = undefined;
+// // const nullVariable = null;
+// // const zeroVariable = 0;
+
+// // console.log('nullVariable > ', nullVariable);
+
+
+
+// // let anyVar = null;
+
+
+// // anyVar = 100;
+
+// // const id1 = Symbol("newID");
+// // console.log('id1 > ', id1);
+
+
+
+
+
+// // string
+// // number
+
+// // boolean
+// // null
+// // undefined
+
+// // BigInt
+// // Symbol
+
+// // Object
+
+
+// // const newObject = {};
+// // const newObject2 = {
+// //     'key': 'value'
+// // };
+
+// // console.log('newObject > ', newObject);
+// // console.log('newObject2 > ', newObject2);
+
+
+// // const array1 = [];
+// // const array2 = [1, 3, 4];
+
+
+// // console.log('array1 > ', array1);
+// // console.log('array2 > ', array2);
+
+
+
+
+// // const a = 1000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // typeof a
+// // console.log(typeof a);
+
+
+
+
+// // const boolVar = false;
+// // console.log(typeof boolVar);
+
+// // const numberVar = 123;
+// // console.log(typeof numberVar);
+
+
+// // const numberString = '123123';
+// // console.log(typeof numberString);
+
+
+// // const objString = {};
+// // console.log(typeof objString);
+
+
+// // console.log(Boolean(1000));
+// // console.log(Number('1000'));
+// // console.log(String(1000));
+// // console.log(parseInt('153abcdgsdfg'));
+// // console.log(parseFloat('433.4dfagmdslfgkn;sdlfng'));
+
+
+
+// // const a = 100;
+// // console.log(+a);
+
+
+
+// // const str = '1000';
+// // console.log(str);
+// // console.log(+str);
+
+// // const aaa = +'50' + 50;
+// // console.log(aaa);
+
+
+
+// // const str1 = 'First half of string';
+// // const str2 = 'Second half of string';
+
+// // // const strConcat = str1 + ' another value ' + str2 + ' manual string';
+
+// // const strConcat = str1 + ' ' + null + ' another value ' + 100 + ' ' + true + ' ' + str2 + ' manual string';
+
+// // console.log('strConcat > ', strConcat);
+
+
+// // !!
+
+
+// // +string
+// // string + number
+// // !!anyValue
+
+
+
+// // const x = 5 + 8;
+// // const y = 5 - 8;
+// // const x1 = 5 * 8;
+// // const y2 = 5 / 8;
+
+// // const xx = 62 + 35 / (25 * 15) % 10 + 17;
+
+// const x3 = 21 % 8;
+// // console.log(x3);
+
+
+
+
+// // let x = 1;
+// // x++;
+// // x++;
+// // x--;
+// // x--;
+// // x--;
+// // x--;
+// // x--;
+// // console.log(x);
+
+
+
+// // let x = 1;
+// // ++x;
+// // ++x;
+// // --x;
+// // --x;
+// // --x;
+// // --x;
+// // --x;
+// // console.log(x);
+
+
+// // let x = 1;
+// // console.log(++x);
+// // console.log(x++);
+// // console.log(x);
+
+
+// // const a = 4;
+// // const b = 8;
+// // console.log(a < b);
+// // console.log(a > b);
+// // console.log(a <= b);
+// // console.log(a >= b);
+// // console.log(a == b);
+// // console.log(a != b);
+
+// // console.log(a === b);
+// // console.log(a !== b);
+
+
+// // const A = true;
+// // const B = false;
+
+// // const C = !A;
+// // console.log('C > ', C);
+// // console.log('B > ', !!B);
+
+
+// // console.log(false && false); // false
+// // console.log(false && true); // false
+// // console.log(true && false); // false
+// // console.log(true && true); // true
+
+// // console.log(false || false); // false
+// // console.log(false || true); // true
+// // console.log(true || true); // true
+// // console.log(true || true); // true
+
+
+// // console.log(false && false && true && false);
+// // console.log(false || false || true || false);
+
+// // console.log(true && false);
+// // console.log(true || false);
+
+
+// // const x = 1000;
+// // const a = b = c = x;
+// // console.log(a, b, c);
+
+
+
+
+// // let a = 100; // 100
+// // console.log(a);
+
+// // a = a + 10; // 110
+// // console.log(a);
+
+// // a = a + 20; // 130
+// // console.log(a);
+
+
+
+// // let a = 100; // 100
+// // console.log(a);
+
+// // // a = a + 10; // 110
+// // a += 10; // 110
+// // console.log(a);
+
+// // a += 20; // 130
+// // console.log(a);
+
+
+
+// // let a = 100; // 100
+// // console.log(a);
+
+// // // a = a + 10; // 110
+// // a -= 10; // 110
+// // console.log(a);
+
+// // a -= 20; // 130
+// // console.log(a);
+
+
+// // строка + number + number =  завжди строка, бо конкатенація
+// // 'number' + (23 + 32) // 'number2332'
+
+
+// // // кокатенація призводить все до строки
+// // 41 + 1 + 'number' // '411number'
+
+// // null + 1
+
+// // 'five' + + 'two'
+
+// // 2 && 7
+// // +'40' + +'2';
+// // '10' - 5 === 6;
+// // true + false
+// // '4px' - 3
+// // '4' - 3
+// // '2' + 3 ** 2;
+// // 12 / '6'
+// // 23 + 42 + 'number'
+// // '10' + (5 === 6);
+// // 'number' + 15 + 3
+
+
+
+
+// // const first = '123';
+// // let second_Number = 123;
+
+// // $new = 'new';
+
+// // const name1 = 'John';
+// // const name2 = 'Michael';
+
+// // const name = 'Alert';
+// // // const new = 'Alert';
+
+// // console.log('name > ', name);
+// // // console.log('name > ', new);
+
+// // const button = null;
+// // const btn = null;
+
+// // const snake_case = null;
+
+// // const bigNumber = 999;
+// // const newText = 'New text here';
+
+// // const isFalse = true;
+// // const isVisible = false;
+
+// // const item = null;
+// // const items = [null, null, null];
+
+
+// // const newVar = null;
+// // // newVar = null;
+
+// // const IS_MOBILE = window.innerWidth < 1200 ? true : false;
+
+// // let newVar2 = null;
+// // newVar2 = 100;
+// // console.log('newVar2 > ', newVar2);
+
+
+
+// // {
+// //     alert('234');
+// // }
+
+// // {
+// //     alert('88');
+// // }
+
+
+// const newConst = 'I am new const!';
+
+// {
+//     const newConst = 'New const';
+
+//     {
+//         // console.log('newConst > ', newConst);
+
+//         {
+//             // console.log('newConst > ', newConst);
+//         }
+//     }
+// }
+
+// // console.log('newConst > ', newConst);
+
+
+
+// // var newVar = 'New var 1';
+
+// // {
+// //     var newVar = 'New var 2';
+// //     console.log('newVar > ', newVar);
+
+// //     {
+// //         console.log('newVar > ', newVar);
+// //     }
+// // }
+
+// // console.log('newVar > ', newVar);
+
+
+// // let newLet = 1;
+// // let newLet = 2;
+
+// // var newVar = 1;
+// // var newVar = 2;
+// // console.log(newVar);
+
+
+
+
+// // let first = '123';
+// // const second = 99;
+// // var third = null;
+
+// // fourth = '4';
+
+// // {
+// //     const localConst = null;
+// // }
+
+
+
+
+// // console.log(testVar);
+// // var testVar = 55;
+
+
+
+// // console.log(testLet);
+// // let testLet = 66;
+
+
+// // (5 > 4) => true
+
+// // if (true) {
+// //     alert('TRUE');
+// // }
+
+// // if (false) {
+// //     alert('FALSE');
+// // }
+
+
+// // const a = 10;
+// // const b = 5;
+
+
+// // if (a > b) {
+// //     alert('TRUE');
+// // }
+
+// // if (a < b) {
+// //     alert('A < B');
+// // } else if (a > b) {
+// //     alert('A > B');
+// // } else {
+// //     alert('A = B');
+// // }
+
+// // if (10 > 8) {
+// //     alert('10 > 8');
+// // }
+
+// // alert('Some message');
+
+
+// // const age = +prompt('Скільки вам років?');
+// // console.log('age > ', age);
+
+// // if (age >= 18) {
+// //     console.log('Повнолітній');
+// // } else {
+// //     console.log('Не повнолітній');
+// // }
+
+
+// // if (!null) {
+// //     alert('NULL');
+// // }
+
+// // let index = 0;
+
+// // if (++index) {
+// //     alert('0');
+// // }
+
+// // if ("".length !== 0) {
+// //     alert('___');
+// // }
+
+
+
+// // const number = 10;
+
+// // function customLog(message = '') {
+// //     console.log('********************');
+// //     console.log(message);
+// //     console.log('********************');
+// // }
+
+
+
+// // if (number > 0 && number % 2 === 0) {
+// // 	customLog('Положительное четное число');
+// // } else if (number > 0) {
+// // 	customLog('Положительное нечетное число');
+// // } else if (number < 0) {
+// // 	customLog('Отрицательное число');
+// // } else {
+// // 	customLog('Ноль');
+// // }
+
+
+// // let year = +prompt('Какого года ты рождения?');
+// // let age, a;
+// // age = 2024 - year;
+
+// // if (age < 0) {
+// //     alert(`Ты еще не родился`);
+// // } else if (age === 1 && age === 21 && age === 31 && age === 41 && age === 51) {
+// //     alert(`Тебе : ${age} год`);
+// // } else if (age > 1 && age < 5 || age > 21 && age < 26 || age > 31 && age < 36) {
+// //     alert(`Тебе : ${age} года`);
+// // } else if (age >= 22 && age <= 24) {
+// //     alert(`Тебе : ${age} года`);
+// // }
+
+
+// // let year2 = +prompt('Ваш возраст:');
+// // let ageTxt, age3;
+// // age = 2024 - year;
+
+// // if (year < 0) {
+// //     alert('Возраст не может быть меньше 0');
+// // } else {
+// //     if (age % 10 === 1 && (age % 100 < 11 || age % 100 > 14)) {
+// //         ageTxt = 'год';
+// //     } else if ((age % 10 === 2 || age % 10 === 3 || age % 10 === 4) && (age % 100 < 11 || age % 100 > 14)) {
+// //         ageTxt = 'года';
+// //     } else {
+// //         ageTxt = 'лет';
+// //     }
+// // }
+
+// // alert(`Тебе: ${age} ${ageTxt}`);
+
+
+// // if () {
+
+// // // }
+
+// // const age = prompt('Enter your age');
+
+
+
+// // // if (age >= 18) {
+// // //     alert('18');
+// // // } else {
+// // //     alert('Not 18!');
+// // // }
+
+// // const is18 = (age >= 18) ? '18' : 'Not 18!';
+// // // alert(is18);
+
+
+// // (age >= 18) ? alert('18!') : alert('NOT 18!')
+
+
+
+
+// // const fruitName = prompt('Enter fruit name:');
+
+// // let type;
+
+// // switch (fruitName) {
+// // 	case 'apple': {
+// //         type = 'apple fruit';
+// // 		break;
+// //     }
+// // 	case 'pear':
+// // 	case 'kiwi': {
+// // 		type = 'Fruit';
+// //         alert('!!!!!');
+// // 		break;
+// //     }
+// // 	case 'tomato':
+// // 	case 'cucumber':
+// // 		type = 'Vegetable';
+// // 		break;
+// // 	default:
+// // 		type = 'Unknown';
+// // }
+
+// // alert(type);
+
+
+
+// // {
+// //     console.log('Previous instructions');
+
+// // }
+
+
+// // {
+// //     try {
+// //         const userInput = prompt('Введіть число: ');
+// //         const parsedInput = parseInt(userInput);
+
+// //         if (Number.isNaN(parsedInput)) {
+// //             throw new Error('Введено NaN значення');
+// //         }
+
+// //     } catch (error) {
+// //         console.error('Сталася помилка: ' + error.message);
+// //     }
+// // }
+
+
+// // console.log('Further instructions');
+
+
+
+
+
+
+// console.log('Any message');
+// console.log('Any message');
+// console.log('Any message');
+// console.log('Any message');
+// console.log('Any message');
+// console.log('Any message');
+// console.log('Any message');
+
+
+// for (let i = 0; i < 7; i++) {
+//     console.log('Any message');
+// }
+
+
+
+// let i = 0;
+
+
+// while (i < 15) {
+//     console.log('Any message' + ' > ' + i);
+
+//     i++;
+// }
+
+
+
+
+
+// 5! = 1 * 2 * 3 * 4 * 5
+// 8! = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8
+// 9! = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9
+// 3! = 1 * 2 * 3
+
+
+
+// let i = 1;
+// let factorial = 1;
+// const target = +prompt('Enter factorial: ', 1);
+// //     1       5
+// while (i <= target)  {
+//     factorial = factorial * i;
+
+//     i++;
+// }
+
+// // console.log('factorial = ', factorial);
+
+
+
+
+// while (true) {
+//     console.log('true');
+// }
+
+
+
+
+
+
+// let i = 0;
+
+
+// while (i < 15) {
+//     console.log('Any message' + ' > ' + i);
+
+//     i++;
+// }
+
+
+
+
+
+// let i = 0;
+
+// do {
+//     console.log('Any message' + ' > ' + i);
+
+//     i++;
+// } while (i < 15);
+
+
+
+// let userInput = +prompt('Enter number: ');
+
+// while (userInput > 0) {
+//     console.log('userInput > ', userInput);
+
+//     userInput = +prompt('Enter number: ');
+
+
+// }
+
+
+
+// let userInput;
+
+// do {
+//     userInput = +prompt('Enter number: ');
+
+//     if (userInput > 0) {
+//         console.log('userInput > ', userInput);
+//     }
+// } while (userInput > 0);
+
+
+
+
+// let i = 0;
+
+// while (i < 10) {
+// //     console.log('userInput > ', userInput);
+
+// //     userInput = +prompt('Enter number: ');
+
+//     i++;
+// // }
+
+
+// for (initialiation; condition; step) {
+// execution
+// }
+
+
+
+
+// let i = 1;
+// let factorial = 1;
+// const target = +prompt('Enter factorial: ', 1);
+// //     1       5
+// while (i <= target)  {
+//     factorial = factorial * i;
+
+//     i++;
+// }
+
+// console.log('factorial = ', factorial);
+
+
+
+
+
+// const target2 = +prompt('Enter factorial: ', 1);
+// let factorial2 = 1;
+
+// for (let i = 1; i <= target2; i++) {
+//     factorial2 = factorial2 * i;
+// }
+
+// console.log('factorial2 = ', factorial2);
+
+
+
+
+
+// const target2 = +prompt('Enter factorial: ', 1);
+// let factorial2 = 1;
+
+// // const target2 = 5;
+
+// // const radix = 3;
+
+
+// for (let i = 1; i <= target2; i++) {
+//     factorial2 = factorial2 * i;
+//     console.log('-----------------------');
+
+// }
+
+// console.log('factorial2 = ', factorial2);
+
+
+
+// const a = c = 5, b = 6;
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// let first, second, third = null;
+
+
+
+// [1; 100]
+
+// 1, 2, 3, 45, 5,
+
+
+
+
+// Знайдіть суму всіх чисел від 1 до 100
+// які не кратні ані 3, ані 5
+
+
+
+// let sum = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 !== 0 && i % 5 !== 0) {
+//         sum += i;
+//     }
+// }
+
+// console.log(sum);
+
+
+
+
+
+
+
+// for (let i = 1; i <= 100; i += 4) {
+
+//     console.log(i);
+// }
+
+
+
+
+// for in
+
+
+// const user = {
+//     name: 'Roman',
+//     lastName: 'Yaremiy',
+//     job: 'programmer',
+//     height: 180,
+//     20: 'any string',
+//     5: 'some value',
+// }
+
+// // console.log(user);
+
+
+// for (let key in user) {
+//     console.log(key + ': ' + user[key]);
+// }
+
+
+
+
+// for of
+
+
+// const myArray = [4, 6, 7, 9];
+
+// for (let value of myArray) {
+//     console.log(value);
+// }
+
+
+
+// const paragraphs = document.querySelectorAll('.paragraph');
+// console.log('paragraphs > ', paragraphs);
+
+
+// for (let par of paragraphs) {
+//     console.log(par);
+// }
+
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+
+// for (let k = 10; k >= 1; k--) {
+//     console.log(k);
+// }
+
+
+// let j = 16;
+
+// while (j >= 1) {
+//     console.log(j);
+
+//     j--;
+// }
+
+
+
+
+// for (let i = 0; i <= 155; i++) {
+//     if (i % 2 === 0) continue;
+
+//     console.log(i);
+// }
+
+
+
+
+// for (let i = 0; i <= 15; i++) {
+//     console.log(i);
+
+//     if (i === 5) break;
+// }
+
+
+
+// const myArray = [
+//     ['a', 'b', 'c'],
+//     ['1', '2', '3'],
+//     [4, 5, 6],
+// ];
+
+// // console.log(myArray);
+// // console.log(myArray.length);
+
+// outerLoop: for (let i = 0; i < myArray.length; i++) {
+
+//     for (let j = 0; j < myArray[i].length; j++) {
+//         if (j === '2') continue outerLoop;
+
+//         console.log(myArray[i][j]);
+//     }
+// }
+
+
+
+
+
+
+// // for =>
+// console.log('  *  ');
+// console.log(' *** ');
+// console.log('*****');
+
+
+
+
+
+
+// const treeHeight = prompt(); // 5
+
+let christmasTree = ``;
+
+
+for (let i = 0; i < treeHeight; i++) {
+
+    += 2;
+
+    christmasTree += `  *  \r\n`; // 1
+    christmasTree += ` *** \r\n`; // 2
+    christmasTree += `*****\r\n`; // 3
+    // ...
+
+// }
+
+console.log(christmasTree);
